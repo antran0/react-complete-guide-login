044a634d320421b92460877fc2d0fda3e5ede03a
Steven Tran <steven.tran550@gmail.com>
2023-04-06 20:07:22 -0700

    Lecture 124 - building & using a custom context provider component.
    Refactored the logging in logic into auth-context store so that
    components that need the isLoggedIn state can use it through the
    Context API instead of through props.
    
    We can place logic that is used is many different components in one
    centralized location by creating a component that encapsulates a context
    provider such as:
      const MyContext = React.createContext({
        someState: initialValue,
        someFunction: (args) => {}
      });
    
      export const MyContextProvider = (props) => {
        const [myState, setMyState] = React.useState({initial state});
        const someFunction = (newState) => {
          setMyState(newState);
        };
    
        return (
          <MyContext.Provider value={{
            myState: myState,
            someFunction: someFunction
          }}>
            {props.children}
          </MyContextProvider>
        );
      };
    
      export default MyContext;
    
    Components anywhere in the component tree that need data stored in this
    context can import this file and call the useContext hook to access
    the data:
      import MyContext from "fileWithContext.js";
    
      const SomeComponent = (props) => {
        const myContext = React.useContext(MyContext);
        const someState = myContext.someState;
        const someFunction = myContext.SomeFunction;
    
        return (<div></div>);
      };
    
    The lowest common ancestor of all components that require some context
    data should be wrapped with the custom provider component.
      // in index.js (if all components need some context)
      ...
      root.render(
        <MyContextProvider>
          <App />
        </MycontextProvider>
      );

--- code/01-starting-project/src/App.js
+++ code/01-starting-project/src/App.js
@@ -1,4 +1,4 @@
-import React, { useState, useEffect } from "react";
+import React, { useContext } from "react";
 
 import Login from "./components/Login/Login";
 import Home from "./components/Home/Home";
@@ -6,44 +6,15 @@ import MainHeader from "./components/MainHeader/MainHeader";
 import AuthContext from "./store/auth-context";
 
 function App() {
-  const [isLoggedIn, setIsLoggedIn] = useState(false);
-
-  // useEffect(function, [dependencies...])
-  useEffect(() => {
-    // This function runs once after initial component render AND if any
-    // dependencies in list given as the second argument to useEffect
-    // changes.
-    // If no dependencies are given, this function only runs after the initial
-    // render.
-    const storedUserLoggedInInformation = localStorage.getItem("isLoggedIn");
-
-    if (storedUserLoggedInInformation === "1") {
-      setIsLoggedIn(true);
-    }
-  }, []);
-
-  const loginHandler = (email, password) => {
-    // We should of course check email and password
-    // But it's just a dummy/ demo anyways
-    localStorage.setItem("isLoggedIn", "1");
-    setIsLoggedIn(true);
-  };
-
-  const logoutHandler = () => {
-    localStorage.removeItem("isLoggedIn");
-    setIsLoggedIn(false);
-  };
-
+  const ctx = useContext(AuthContext);
   return (
-    <AuthContext.Provider
-      value={{ isLoggedIn: isLoggedIn, onLogout: logoutHandler }}
-    >
+    <React.Fragment>
       <MainHeader />
       <main>
-        {!isLoggedIn && <Login onLogin={loginHandler} />}
-        {isLoggedIn && <Home onLogout={logoutHandler} />}
+        {!ctx.isLoggedIn && <Login />}
+        {ctx.isLoggedIn && <Home />}
       </main>
-    </AuthContext.Provider>
+    </React.Fragment>
   );
 }
 
